406e2a904444f3385c243a1f3da2d3a1332bbe7f
Łukasz <luk.tucholski@gmail.com>
2019-10-25 21:45:58 +0200

    added axios and configure default url and auth key for openweathermap api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router/index.js';
-
+import axios from 'axios'
 import './styles/index.scss';
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://api.openweathermap.org/data/2.5/';
+axios.defaults.params = { APPID: 'cc1784c705369804b4f0145167a57987' };
+// btw, axios in v0.19 have bug with default params so need to use older package.
 
 new Vue({
   el: '#app',
